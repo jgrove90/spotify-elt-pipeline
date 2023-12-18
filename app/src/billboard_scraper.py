c1e5b92ff7fe612948bb53e840bbc25057e1bcca
jared <jgrove90@gmail.com>
2023-12-18 13:47:45 -0700

    Implement get_year_end_hot_100_songs function

--- app/src/billboard_scraper.py
+++ app/src/billboard_scraper.py
@@ -1,3 +1,7 @@
+import requests 
+from bs4 import BeautifulSoup 
+import re
+
 def get_year_list(start_year:int, end_year:int) -> list[int]:
     """   
     Generates a list of years starting from "start_year"
@@ -14,4 +18,40 @@ def get_year_list(start_year:int, end_year:int) -> list[int]:
 
     return year_list
 
-    
+def get_year_end_hot_100_songs(years:list) -> list[tuple]:
+    """ 
+        Collects Billboard Year-End Hot 100 Songs data for the specified years.
+
+        Args:
+            years (list): A list of integers representing the years for which data is to be collected.
+
+        Returns:
+            list: A list of tuples containing the Billboard Year-End Hot 100 Songs data.
+    """
+    collected_data = []
+
+    for year in years:
+        print(f"Gathering {year} Billboard Year-End Hot 100 Songs.")
+        url = f"https://www.billboard.com/charts/year-end/{year}/hot-100-songs/"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, features='html.parser')
+        
+        # extract main container to search for data
+        container_element = soup.find("div", class_="pmc-paywall")
+        
+        # search data of interest
+        ranks = container_element.find_all("span", class_=re.compile(r".*c-label a-font-primary-bold-l.*"))
+        artists = container_element.find_all("span", class_=re.compile(r".*c-label a-font-primary-s.*"))
+        songs = container_element.find_all("h3", id="title-of-a-story")
+
+        # collect data        
+        for rank, artist, song in zip(ranks, artists, songs):
+            rank_text, artist_text, song_text = rank.text.strip(), artist.text.strip(), song.text.strip()
+            
+            billboard_data = (int(rank_text), song_text, artist_text, year)
+            
+            collected_data.append(billboard_data)
+            
+    return collected_data
+
+
